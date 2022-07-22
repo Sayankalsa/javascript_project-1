de0b9dd49e7796e440605b6d6fa61093c90b8d3b
Sayankalsa <kalsasayan007@gmail.com>
2022-07-22 21:00:09 +0530

    practice js project

---
+++ app.js
@@ -0,0 +1,16 @@
+const closedFace = document.getElementById('closed');
+const openFace = document.getElementById('open');
+
+closedFace.addEventListener('click', () => {
+    if (openFace.classList.contains('open')) {
+        openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+});
+
+openFace.addEventListener('click', () => {
+    if (closedFace.classList.contains('closed')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
+    }
+});
